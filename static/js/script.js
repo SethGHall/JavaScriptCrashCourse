7e1454c04cebbbd19880e7f8ca55b6cb0c09aa44
Seth Hall <seth.george.hall@gmail.com>
2022-12-10 13:43:21 +1300

    updated challenge for rock paper scissors

--- static/js/script.js
+++ static/js/script.js
@@ -31,3 +31,112 @@ function removeCat()
         element.removeChild(element.lastChild);
 }
 
+function generateRockPaperScissors()
+{
+    let x = Math.floor(Math.random()*3.0);
+    return['rock','paper','scissors'][x];
+}
+
+
+const rpsGameChoices = {
+    rock: {scissors:1, rock:0, paper:-1}, 
+    paper:{rock:1, paper:0, scissors:-1},
+    scissors:{paper:1, scissors:0, rock:-1}
+};
+
+function runRPSGame(userpick)
+{   
+    console.log(userpick.id);
+    var botChoice = generateRockPaperScissors();
+    var userChoice = userpick.id;
+    console.log(botChoice);
+
+    var winner = rpsGameChoices[userChoice][botChoice];
+    var message = getMessage(winner, userChoice, botChoice);
+
+    console.log(winner); 
+    console.log(message);
+
+    displayGameState(userChoice,botChoice,message);
+
+
+}
+function displayGameState(userChoice, botChoice, message)
+{
+    var rockImg = document.getElementById("rock");
+    var paperImg = document.getElementById("paper");
+    var scissorsImg = document.getElementById("scissors");
+
+    var images = {
+        rock: rockImg.src,
+        paper: paperImg.src,
+        scissors: scissorsImg.src
+    };
+
+    rockImg.remove();
+    paperImg.remove();
+    scissorsImg.remove();
+
+    var container = document.getElementById("rpsgamecontainer");
+
+    var imgElementUser = document.createElement("img")
+    imgElementUser.src = images[userChoice];
+    imgElementUser.width=rockImg.width;
+    imgElementUser.height=rockImg.height;
+    imgElementUser.onclick = function(){resetContainer(rockImg,paperImg,scissorsImg)};
+    imgElementUser.style="box-shadow:0px 10px 50px rgba(0,0,255,50)";
+    
+    container.appendChild(imgElementUser);
+
+    var botElementUser = document.createElement("img")
+    botElementUser.src = images[botChoice];
+    botElementUser.width=rockImg.width;
+    botElementUser.height=rockImg.height;
+    botElementUser.onclick = function(){resetContainer(rockImg,paperImg,scissorsImg)};
+
+    botElementUser.style="box-shadow:0px 10px 50px rgba(255,0,0,50)";
+
+    var textDiv = document.createElement("textDiv");
+
+    var text = document.createElement("h2");
+    text.style.color= message.color;
+    text.innerText = message.message;
+
+    textDiv.appendChild(text);
+
+    container.appendChild(textDiv);
+    container.appendChild(botElementUser);
+
+}
+
+function resetContainer(rockImg,paperImg,scissorsImg)
+{
+    console.log("Resetting Container");
+    container = document.getElementById("rpsgamecontainer");
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    container.appendChild(rockImg);
+    container.appendChild(paperImg);
+    container.appendChild(scissorsImg);
+}
+
+function getMessage(winner, userChoice, botChoice)
+{
+    var message = {message: null, color: null};
+
+    if(winner == 1)
+    {   message["message"] = "You Won! "+userChoice+" beats "+botChoice;
+        message['color'] = "green";
+    }
+    else if(winner == -1)
+    {   message["message"] = "You Lost! "+userChoice+" lost to "+botChoice;
+        message['color'] = "red";
+    }
+    else
+    {
+        message["message"] = "Draw! "+"both picked "+userChoice;
+        message['color'] = "blue";
+    }
+    return message;
+} 
